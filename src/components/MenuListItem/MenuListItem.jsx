5e932050b50b6b3993567fa032f8e37d30152ed1
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-09-16 12:55:09 -0700

    Set up account page and components

---
+++ src/components/MenuListItem/MenuListItem.jsx
@@ -0,0 +1,13 @@
+import './MenuListItem.scss';
+
+function MenuListItem() {
+    return (
+        <section className="menu-item">
+            <div></div>
+            <h2></h2>
+            <div></div>
+        </section>
+    );
+}
+
+export default MenuListItem;
---
+++ src/pages/Account/AccountPage.jsx
@@ -0,0 +1,11 @@
+import './AccountPage.scss';
+
+function AccountPage() {
+    return (
+        <section className="account">
+            <h1 className="account__title">My Account</h1>
+        </section>
+    );
+}
+
+export default AccountPage;
