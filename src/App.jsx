6c4693f8f41cc34a8b6767e72cf666c1773c1510
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-09-16 12:40:44 -0700

    Set up router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
