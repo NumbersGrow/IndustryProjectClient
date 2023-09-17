7530c9d68f368f911be552ed4f442aad5b74d1b1
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-09-17 12:31:48 -0700

    Completed cart component and fine-tuned page styles

--- src/components/Cashier/Cart/Cart.jsx
+++ src/components/Cashier/Cart/Cart.jsx
@@ -13,7 +13,29 @@ function Cart() {
 
             <div className="cart__products">
                 <ProductInCart />
+                <ProductInCart />
+                <ProductInCart />
             </div>
+            <div className="cart__summary">
+                <h3 className="cart__summary-title">Summary</h3>
+                <div className="cart__summary-info">
+                    <p className="cart__summary-info-name">Subtotal</p>
+                    <p className="cart__summary-info-cost">$0.00</p>
+                </div>
+                <div className="cart__summary-info">
+                    <p className="cart__summary-info-name">GST (5%)</p>
+                    <p className="cart__summary-info-cost">$0.00</p>
+                </div>
+                <div className="cart__summary-info">
+                    <p className="cart__summary-info-name">PST (7%)</p>
+                    <p className="cart__summary-info-cost">$0.00</p>
+                </div>
+                <div className="cart__summary-info cart__summary-info-total">
+                    <p className="cart__summary-info-name">Total</p>
+                    <p className="cart__summary-info-cost">$0.00</p>
+                </div>
+            </div>
+            <button className="cart__button">Next</button>
         </section>
     )
 }
--- src/components/Cashier/Cart/ProductInCart.jsx
+++ src/components/Cashier/Cart/ProductInCart.jsx
@@ -5,11 +5,11 @@ function ProductInCart() {
         <section className="cart-product">
             <img className="cart-product__pic" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png" alt="placeholder image" />
             <div className="cart-product__info">
-                <h3 className="cart-product__name">cart-product Name</h3>
+                <h3 className="cart-product__name">Product Name</h3>
                 <p className="cart-product__category">Category</p>
                 <div className="cart-product__counter">
                     <div className="cart-product__counter-reduce">-</div>
-                    <div className="cart-product__counter-quantity">0</div>
+                    <div className="cart-product__counter-quantity">2</div>
                     <div className="cart-product__counter-increase">+</div>
                 </div>
             </div>
