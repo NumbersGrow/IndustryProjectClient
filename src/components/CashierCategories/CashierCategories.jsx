d61c7c9bd5013443d0a5695ce9fcdf6dba039e32
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-09-16 19:02:05 -0700

    Created categories list

---
+++ src/components/CashierCategories/CashierCategories.jsx
@@ -0,0 +1,53 @@
+import './CashierCategories.scss';
+
+function CashierCategories() {
+    return (
+        <section className="categories">
+            <h2 className="categories__title">Categories</h2>
+            <div className="categories__list">
+                <div className="categories__list-item">
+                    <div className="categories__list-item-pic">
+
+                    </div>
+                    <div className="categories__list-item-name">
+                        Category Name
+                    </div>
+                </div>
+                <div className="categories__list-item">
+                    <div className="categories__list-item-pic">
+
+                    </div>
+                    <div className="categories__list-item-name">
+                        Category Name
+                    </div>
+                </div>
+                <div className="categories__list-item">
+                    <div className="categories__list-item-pic">
+
+                    </div>
+                    <div className="categories__list-item-name">
+                        Category Name
+                    </div>
+                </div>
+                <div className="categories__list-item">
+                    <div className="categories__list-item-pic">
+
+                    </div>
+                    <div className="categories__list-item-name">
+                        Category Name
+                    </div>
+                </div>
+                <div className="categories__list-item">
+                    <div className="categories__list-item-pic">
+
+                    </div>
+                    <div className="categories__list-item-name">
+                        Category Name
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default CashierCategories;
