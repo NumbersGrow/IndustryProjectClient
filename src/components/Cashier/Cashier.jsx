6cbf27c8eb8cf1f4d2a3eb34955d3fd9e45a3d9a
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-09-16 18:14:44 -0700

    Created cashier profile component

---
+++ src/components/Cashier/Cashier.jsx
@@ -0,0 +1,13 @@
+import './Cashier.scss';
+
+function Cashier() {
+    return (
+        <section className="cashier">
+            <h2 className="cashier__name">Name I</h2>
+            <h3 className="cashier__position">Position</h3>
+            <img className="cashier__profile-pic" src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" alt="placeholder profile" />
+        </section>
+    )
+}
+
+export default Cashier;
