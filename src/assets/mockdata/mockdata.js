087a41a97fa1b9bf85320f26a8478e4c09071bad
Maria Zak <mzakart@gmail.com>
2023-09-18 18:32:56 -0400

    Added JS counter, placement for product section

---
+++ src/assets/mockdata/mockdata.js
@@ -0,0 +1,36 @@
+const mockData = [
+    {
+        id: 1,
+        name: "Organic Soap",
+        category: "Bath & Body",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png",
+        price: 12.00,
+        quantity: 2
+    },
+    {
+        id: 2,
+        name: "Hand Lotion",
+        category: "Bath & Body",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png",
+        price: 8.00,
+        quantity: 1
+    },
+    {
+        id: 3,
+        name: "Shampoo Bar",
+        category: "Hair Care",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png",
+        price: 10.00,
+        quantity: 3
+    },
+    {
+        id: 4,
+        name: "Conditioner",
+        category: "Hair Care",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png",
+        price: 9.50,
+        quantity: 1
+    }
+];
+
+export default mockData;
