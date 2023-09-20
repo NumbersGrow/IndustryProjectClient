2051c72f97bd8184c1508bf9e1d303da80c22533
Maria Zak <mzakart@gmail.com>
2023-09-19 21:18:28 -0400

    Added button to images

--- src/components/Customer/MobileProduct/MobileProduct.js
+++ src/components/Customer/MobileProduct/MobileProduct.js
@@ -8,12 +8,15 @@ function MobileProduct() {
 
     return (
         <section className="mobile-cart-product">
+            <div className="mobile-cart-product__pic-container">
             <img className="mobile-cart-product__pic" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png" alt="placeholder image" />
+           <div className="mobile-cart-product__pic-container-add">+</div>
+           </div>
+           
             <div className="mobile-cart-product__info">
 
             <div className="mobile-cart-product__info-top">
             <h3 className="mobile-cart-product__name">Product Name</h3>
-            <p className="mobile-cart-product__category">Category</p>
             </div>
             <div className="mobile-cart-product__info-bottom">
           
