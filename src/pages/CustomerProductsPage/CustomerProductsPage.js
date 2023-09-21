2c56aa48aa26f0210dc92ecc19ebfcaebdcd6c10
Maria Zak <mzakart@gmail.com>
2023-09-21 19:01:07 -0400

    pic

--- src/pages/CustomerProductsPage/CustomerProductsPage.js
+++ src/pages/CustomerProductsPage/CustomerProductsPage.js
@@ -2,6 +2,9 @@ import './CustomerProductsPage.scss';
 import image1 from "../../assets/images/lord_of_misrule_bath_bomb_ecom_2023_3accf742_thumbnail_256.png";
 import image2 from "../../assets/images/punkin_pumpkin_bath_bomb_2023_724062ec_thumbnail_256.png";
 import image3 from "../../assets/images/ghostie.avif";
+import image6 from "../../assets/images/snowdrift_bath_bomb_2023_f2e20180_thumbnail_256.png";
+import image5 from "../../assets/images/sweet_pudding_bath_bomb_2023_fc0cd8f3_thumbnail_256.png";
+import image4 from "../../assets/images/snow_fairy_lights_bath_bomb_gift_2021_78a77f76_thumbnail_256.png";
 import HeaderMobile from "../../components/Customer/HeaderMobile/HeaderMobile";
 import MobileProduct from "../../components/Customer/MobileProduct/MobileProduct";
 import { ReactComponent as ArrowRight } from "../../assets/icons/chevron-right.svg"; 
@@ -18,17 +21,17 @@ function CustomerProductsPage() {
 <h3>Bath Bombs</h3>
 <div className='container-products'> 
 <MobileProduct name="PUMKIN PUMPKIN"  price="$8.00" imgSrc={image1}/>
-<MobileProduct name="ALIEN"  price="$10.00" imgSrc={image2}/>
+<MobileProduct name="ALIEN"  price="$10.00" imgSrc={image6}/>
 <MobileProduct name="LORD OF MISRULE" price="$9.50" imgSrc={image3}/>
-<MobileProduct name="ALIEN"  price="$10.00" imgSrc={image2}/>  
+<MobileProduct name="ALIEN"  price="$10.00" imgSrc={image5}/>  
 </div>
 <div className='container-products__more'><p>See All <ArrowRight className='container-products__icon'/> </p></div>
 
 <h3>Skincare</h3>
 <div className='container-products'> 
-    <MobileProduct name="ALIEN"  price="$10.00" imgSrc={image2}/>
+    <MobileProduct name="ALIEN"  price="$10.00" imgSrc={image4}/>
 <MobileProduct name="ALIEN"  price="$10.00" imgSrc={image2}/>
-<MobileProduct name="LORD OF MISRULE" price="$9.50" imgSrc={image3}/>
+<MobileProduct name="LORD OF MISRULE" price="$9.50" imgSrc={image6}/>
 <MobileProduct  name="PUMKIN PUMPKIN"  price="$8.00" imgSrc={image1}/>
 </div>
 <div className='container-products__more'><p>See All <ArrowRight className='container-products__icon'/> </p></div>
@@ -38,9 +41,9 @@ function CustomerProductsPage() {
 <h3>Bubble Bars</h3>
 <div className='container-products'> 
 <MobileProduct name="PUMKIN PUMPKIN"  price="$8.00" imgSrc={image1}/>
-<MobileProduct name="ALIEN"  price="$10.00" imgSrc={image2}/>
-<MobileProduct name="ALIEN"  price="$10.00" imgSrc={image2}/>
-<MobileProduct name="PUMKIN PUMPKIN"  price="$8.00" imgSrc={image1}/>
+<MobileProduct name="ALIEN"  price="$10.00" imgSrc={image4}/>
+<MobileProduct name="ALIEN"  price="$10.00" imgSrc={image5}/>
+<MobileProduct name="PUMKIN PUMPKIN"  price="$8.00" imgSrc={image6}/>
 </div>
 
 <div className='container-products__more'><p>See All <ArrowRight className='container-products__icon'/> </p></div>
